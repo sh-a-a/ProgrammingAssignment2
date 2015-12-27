a4ea21f1c054bf0a68080426209b95a5f03e9a18
Shaa <shaa@Shaas-MacBook-Pro.local>
2015-12-27 16:55:01 -0400

    Added comments and functions bodies

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## These are two functions which helps you invert matrix.
+## Main difference from original `solve` that `cacheSolve`
+## creates a cache of already inverted matrix, so you will
+## not spend copmuting time to inverse matrix which already
+## have been inverted.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    # Creates a special 'vector' (list) containing functions
+    # to set matrix, get matrix, set inverse matrix and get
+    # inverted matrix
+    # Args:
+    #   x: invertable matrix
+    # Returns:
+    #   list of functions
+    
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
-
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    # Inverting matrix created with `makeCacheMatrix` function
+    # If matrix haven't been `solved` yet it will inverse it
+    # and save `solved` matrix for future use
+    # Args:
+    #   x: object returned by `makeCacheMatrix`
+    #   ...: further arguments passed to `solve` function
+    # Returns:
+    #   matrix that is the inverse of 'x'
+    
+    # Trying to get cached data
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("Getting cached data")
+        return(m)
+    }
+    # No cache data was found, using standard `solve`
+    # and caching it result
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
 }
